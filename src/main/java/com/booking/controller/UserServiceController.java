658c3e708d0ab489d7c3cec392711865c28d4adb
dilipkumarmehta <dilipkumarmehta02@gmail.com>
2020-01-07 08:22:37 +0530

    implementing user service with sql server

--- src/main/java/com/booking/controller/UserServiceController.java
+++ src/main/java/com/booking/controller/UserServiceController.java
@@ -1,35 +1,58 @@
 package com.booking.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.booking.beans.Password;
+import com.booking.beans.Status;
 import com.booking.beans.User;
+import com.booking.service.UserService;
 
 @RestController
 
 public class UserServiceController {
 
-	@RequestMapping(value = "/launching", method = RequestMethod.GET, produces = "application/json", consumes = "application/json")
-	public String launching() {
-		String launchingResponsc = "this responce from launching page";
-		return launchingResponsc;
-
-	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.GET, produces = "application/json", consumes = "application/json")
-	public String login(@RequestBody User user) {
-		String loginResponsc = "this responce from login page";
+	@Autowired
+	UserService userService;
+	Logger logger= LoggerFactory.getLogger(UserServiceController.class);
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	public Status login(@RequestBody User user) {
+		Status loginResponsc = userService.logIn(user);
 		return loginResponsc;
 
 	}
 
-	@RequestMapping(value = "/userdetails", method = RequestMethod.GET, produces = "application/json", consumes = "application/json")
-	public String userDetails() {
-		String userdetailsResponsc = "this responce from login page";
-		return userdetailsResponsc;
+	@RequestMapping(value = "/logout/{userId}", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	public Status logOut(@PathVariable(value = "userid") String userId) {
+		return null;
 
 	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	public Status signUp(@RequestBody User user) {
+		Status signupResponse = userService.signUp(user);
+		return signupResponse;
+
+	}
+
+	@RequestMapping(value = "/changepassword", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	public Status changePassword(@RequestBody Password password) {
+		return userService.changepasswor(password);
+
+	}
+
+	// @RequestMapping(value="/pow", method=RequestMethod.GET)
+	// (@RequestParam(value="base") int base1)
+
+	// @RequestMapping("/sqrt/{num}")
+	// public double sqrt(@PathVariable(value="num") int num1)
+
 
 }
